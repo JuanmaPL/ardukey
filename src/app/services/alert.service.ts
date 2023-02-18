0bd9950facd815635a6f4a5b91dc054c3ca8c7fb
Aythami Déniz Morales <aythami-92@hotmail.com>
2023-02-18 16:49:19 +0000

    Se implementa nueva función en alert service y se trabaja en página de información y ayuda

--- src/app/services/alert.service.ts
+++ src/app/services/alert.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { ok } from 'assert';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,29 @@ export class AlertService {
 
   constructor(private alertCtrl: AlertController) { }
 
-  async registerAlert(status: string,sms: string) {
+  async registerAlert(status: string, sms: string) {
     const alert = await this.alertCtrl.create({
       header: status,
       subHeader: sms,
+      mode: 'ios',
       buttons: ['Listo']
+    });
+
+    await alert.present();
+  }
+
+  async infoAlert(status: string, sms: string, mensaje: string) {
+    const alert = await this.alertCtrl.create({
+      header: status,
+      subHeader: sms,
+      message: mensaje,
+      mode: 'ios',
+      buttons: [
+        {
+          text: 'Ok',
+          role: 'confirm'
+        }
+      ]
     });
 
     await alert.present();
